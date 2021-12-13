128c84981454a1f4e674db71e7b41db6204a575c
unknown <736560054@qq.com>
2021-12-13 23:37:07 +0000

    Unity3D

---
+++ NewProject/Music/Assets/AudioVisable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioVisable : MonoBehaviour
+{
+    public static AudioVisable Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    AudioSource audioSource;
+    public static float[] samples = new float[512];
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ NewProject/Music/Assets/InstanctionCube.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanctionCube : MonoBehaviour
+{
+    public GameObject sampleCubePrefab;
+    GameObject[] sampleCube = new GameObject[512];
+    public float maxScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 512; i++)
+        {
+            GameObject instanceSampleCube = (GameObject)Instantiate (sampleCubePrefab);
+            instanceSampleCube.transform.position = this.transform.position;
+            instanceSampleCube.transform.parent = this.transform;
+            instanceSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            instanceSampleCube.transform.position = Vector3.forward * 100;
+            sampleCube [i] = instanceSampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 512; i++)
+        {
+            if(sampleCube != null)
+            {
+                sampleCube[i].transform.localScale = new Vector3(10, (AudioVisable.samples[i] * maxScale) + 2, 10);
+            }
+        }
+    }
+}
