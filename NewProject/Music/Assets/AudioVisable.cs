ee878b85d86fabea6439ae19c499832b48ccb874
unknown <736560054@qq.com>
2021-12-14 00:32:50 +0000

    Unity3D

--- NewProject/Music/Assets/AudioVisable.cs
+++ NewProject/Music/Assets/AudioVisable.cs
@@ -12,6 +12,7 @@ public class AudioVisable : MonoBehaviour
     }
     AudioSource audioSource;
     public static float[] samples = new float[512];
+    public static float[] freqenceCubes = new float[8];
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +23,36 @@ public class AudioVisable : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        GetFreqenceCubes();
     }
 
     void GetSpectrumAudioSource()
     {
         audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void GetFreqenceCubes()
+    {
+        int count = 0;
+
+        for(int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow (2, i) * 2;
+            if(i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for(int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqenceCubes[i] = average * 10;
+        }
     }
 }
